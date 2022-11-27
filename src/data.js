dca2876729cee0124cbf0db7411f05d79d44b72b
Cristian Murua <cristianmurua1995@gmail.com>
2022-11-27 07:42:05 -0300

    info loaded

--- src/data.js
+++ src/data.js
@@ -1,70 +1,47 @@
 import * as icon  from 'react-icons/si'
 
 const profile = {
-	name: 'Carlos Montenegro',
-	role: 'Front End Developer',
-	description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed facere, itaque excepturi, necessitatibus cum ex nisi.',
+	name: 'Cristian Murua',
+	role: 'Full Stack Developer',
+	description: 'Soy un reciente desarrollador web con inicios de estudios sobre programación durante la pandemia. Con facilidad y preferencia para trabajar en equipo. Soltura en Server y React. Gran predisposición para el continuo aprendizaje y ambición por el perfeccionamiento personal.Actualmente sigo aprendiendo Python para sumar a mi stack y tengo como meta ampliar mis conocimientos en idioma (Ingles: intermedio, Alemán: intermedio).',
 };
 
 const social = [
-	{ icon: icon.SiGithub, link: 'https://github.com' },
-	{ icon: icon.SiCodepen, link: 'https://www.medium.com/' },
-	{ icon: icon.SiTwitter, link: 'https://www.twitter.com/' },
-	{ icon: icon.SiInstagram, link: 'https://www.instagram.com/' },
+	{ icon: icon.SiGithub, link: 'https://github.com/Crismurua' },
+	{ icon: icon.SiLinkedin, link: 'https://www.linkedin.com/in/crismurua/' },
 ];
 
 const brand = 'Singleton'
 
 
 const work = [
+	
 	{
-		name: 'Todo app',
-		description: 'Phasellus metus mi, scelerisque eu volutpat non, posuere et massa. Nunc hendrerit, augue non fringilla mattis.',
+		name: 'E-Commerce - Calathea Market',
+		description: 'E-commerce realizado como proyecto grupal en la academia Henry: Utilizamos la temática de vivero (tienda de plantas e insumos para la jardinería) luego de realizar un breve estudio de mercado en la zona de Rosario, Santa Fe.',
 		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
+			{ icon: icon.SiFirebase, name: 'Firebase' },
+			{ icon: icon.SiExpress, name: 'ExpressJS' },
+			{ icon: icon.SiRedux, name: 'Redux' },
 			{ icon: icon.SiReact, name: 'ReactJS' },
+			{ icon: icon.SiMaterialui, name: 'MaterialUI' },
+			{ icon: icon.SiReacttable, name: 'React Table' },
 		],
-		linkProject: 'https://example.com',
-		linkGithub: 'https://github.com',
+		linkProject: 'https://api-plants-b6153.web.app/',
+		linkGithub: 'https://github.com/Crismurua/Proyecto-Final',
 	},
-	{
-		name: 'Pokedex',
-		description: 'Etiam eget volutpat risus. Nam magna bibendum a metus dignissim, id eleifend libero luctus molestie eget.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://example.com',
-		linkGithub: 'https://github.com',
-	},
-	{
-		name: 'E-Commerce',
-		description: 'Nullam id mauris auctor, sollicitudin sapien id, sagittis turpis proin et nunc sempe, venenatis erat.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://example.com',
-		linkGithub: 'https://github.com',
-	},
-	{
-		name: 'Twitter clon',
-		description: 'Cras leo mauris, gravida vel lectus ac, congue luctus odio. Sed varius varius est eget convallis sagittis sit amet.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://example.com',
-		linkGithub: 'https://github.com',
-	},
+	// {
+	// 	name: 'E-Commerce - Calathea Market',
+	// 	description: 'Nullam id mauris auctor, sollicitudin sapien id, sagittis turpis proin et nunc sempe, venenatis erat.',
+	// 	stack: [
+	// 		{ icon: icon.SiHtml5, name: 'HTML' },
+	// 		{ icon: icon.SiCss3, name: 'CSS' },
+	// 		{ icon: icon.SiJavascript, name: 'Javascript' },
+	// 		{ icon: icon.SiReact, name: 'ReactJS' },
+	// 	],
+	// 	linkProject: 'https://example.com',
+	// 	linkGithub: 'https://github.com',
+	// },
 ]
 
 
@@ -74,26 +51,33 @@ const stack = [
 		items: [
 			{ icon: icon.SiHtml5, name: 'HTML' },
 			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiBootstrap, name: 'Bootstrap', },
+			{ icon: icon.SiLess, name: 'Less', },
+			{ icon: icon.SiMaterialui, name: 'MaterialUI', },
+			{ icon: icon.SiStyledcomponents, name: 'Styled components', },
 			{ icon: icon.SiJavascript, name: 'Javascript' },
 			{ icon: icon.SiTypescript, name: 'Typescript' },
 			{ icon: icon.SiReact, name: 'React', },
+			{ icon: icon.SiRedux, name: 'Redux', },
 		],
 	},
 	{
 		name: 'Back End',
 		items: [
 			{ icon: icon.SiNodedotjs, name: 'NodeJS' },
-			{ icon: icon.SiDeno, name: 'Deno' },
-			{ icon: icon.SiMongodb, name: 'MongoDB', },
+			{ icon: icon.SiExpress, name: 'ExpressJS' },
+			{ icon: icon.SiSequelize, name: 'Sequelize', },
+			{ icon: icon.SiPostgresql, name: 'PostgreSQL', },
+			{ icon: icon.SiMysql, name: 'mySQL', },
 			{ icon: icon.SiFirebase, name: 'Firebase', },
+			{ icon: icon.SiPython, name: 'Python', },
+			{ icon: icon.SiDjango, name: 'Django', },
 		],
 	},
 ]
 
 const contact = {
-	description: 'Tenetur laudantium? Aliquid ipsum perferendis blanditiis non. Adipisci eos numquam reiciendis, rerum, quo similique reprehenderit. Adipisci eos numquam reiciendis',
-	email: 'example@email.com'
+	description: 'Contáctame!',
+	email: 'cristianmurua1995@gmail.com'
 }
 
 export { profile, social, work, stack, contact, brand }
